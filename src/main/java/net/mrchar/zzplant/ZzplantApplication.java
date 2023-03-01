e05f8e243dbd233ac9622bea6c832d59c2a5db7d
mrchar <mrchar@qq.com>
2023-03-01 22:08:37 +0800

    初始化项目

---
+++ src/main/java/net/mrchar/zzplant/ZzplantApplication.java
@@ -0,0 +1,13 @@
+package net.mrchar.zzplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzplantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzplantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/mrchar/zzplant/ZzplantApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mrchar.zzplant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzplantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
